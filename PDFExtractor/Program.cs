fd0775f2bcd2051fac378b48c0676163635e727b
Anaz Zubair <anazzubair@yahoo.com>
2016-11-21 13:02:12 +0400

    Upload processed file to ftp folder.

--- PDFExtractor/Program.cs
+++ PDFExtractor/Program.cs
@@ -7,8 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace ConsoleApplication1
 {
@@ -35,13 +37,44 @@ namespace ConsoleApplication1
 
             var dateSegment = DateTime.Now.ToString().Replace(":", _).Replace(" ", _).Replace("/", _);
             var outputFileName = BEAC + _ + dateSegment + OUTEXTENSION;
-            using (var outStream = System.IO.File.CreateText(outDirectoryName + DIRSEPARATOR + outputFileName))
+            var outputFilePath = outDirectoryName + DIRSEPARATOR + outputFileName;
+            using (var outStream = System.IO.File.CreateText(outputFilePath))
             {
                 foreach (var filePath in Directory.GetFiles(directoryName))
                 {
                     Processfile(filePath, outStream);
                 }
             }
+
+            SendOutputFileToServer(outputFilePath, outputFileName);
+        }
+
+        private static void SendOutputFileToServer(string outputFilePath, string outputFileName)
+        {
+            var host = ConfigurationManager.AppSettings["ftpServer"];
+            var username = ConfigurationManager.AppSettings["ftpUsername"];
+            var password = ConfigurationManager.AppSettings["ftpPassword"];
+            var filePath = ConfigurationManager.AppSettings["ftpFilePath"];
+            var ftpURI = "ftp://" + host + "/" + filePath + "/" + outputFileName;
+            Console.WriteLine("FTP URI: " + ftpURI);
+            var ftpRequest = FtpWebRequest.Create(ftpURI);
+            ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+
+            var sourceStream = new StreamReader(outputFilePath);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            ftpRequest.ContentLength = fileContents.Length;
+
+            Stream requestStream = ftpRequest.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            var response = (FtpWebResponse)ftpRequest.GetResponse();
+
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+
+            response.Close();
         }
 
         private static void SetupOutputDirectory()
